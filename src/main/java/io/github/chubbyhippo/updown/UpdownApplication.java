43433efaa30be517e03bc662dd06f65e36af5304
ch <mkmsacc@outlook.com>
2024-08-07 20:54:47 +0700

    Add initial Spring Boot project setup
    Created a new Spring Boot project with Maven wrapper support. Added essential configuration files including `.gitignore`, `pom.xml`, `application.properties`, and initial Java classes for the application.

---
+++ src/main/java/io/github/chubbyhippo/updown/UpdownApplication.java
@@ -0,0 +1,13 @@
+package io.github.chubbyhippo.updown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpdownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpdownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/chubbyhippo/updown/UpdownApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.chubbyhippo.updown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpdownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
