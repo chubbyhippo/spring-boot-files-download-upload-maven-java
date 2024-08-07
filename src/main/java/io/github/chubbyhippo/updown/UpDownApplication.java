8022352f2b9994a412139bd5c604d6b988bf4c7d
ch <mkmsacc@outlook.com>
2024-08-07 21:00:15 +0700

    Rename main class and test class for consistency
    Renamed `UpdownApplication` to `UpDownApplication` and `UpdownApplicationTests` to `UpDownApplicationTests` for consistent casing across the application. Updated all class references and associated tests accordingly.

--- src/main/java/io/github/chubbyhippo/updown/UpDownApplication.java
+++ src/main/java/io/github/chubbyhippo/updown/UpDownApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UpdownApplication {
+public class UpDownApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UpdownApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UpDownApplication.class, args);
+    }
 
 }
