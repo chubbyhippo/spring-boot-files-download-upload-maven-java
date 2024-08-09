b4d8699aea8ffac831aef7f6f6ae578edba22ec1
chubbyhippo <mkmsacc@outlook.com>
2024-08-09 23:34:33 +0700

    Add StorageFileNotFoundException class
    Introduced a new exception class, StorageFileNotFoundException, that extends StorageException. This class provides constructors for passing an error message and an optional cause, making error handling more specific for file-not-found scenarios.

---
+++ src/main/java/io/github/chubbyhippo/updown/infrastructure/StorageFileNotFoundException.java
@@ -0,0 +1,12 @@
+package io.github.chubbyhippo.updown.infrastructure;
+
+public class StorageFileNotFoundException extends StorageException {
+
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
