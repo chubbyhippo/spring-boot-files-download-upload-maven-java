1aed662ff617f0dd6e39f33e13dfea34b9621638
chubbyhippo <mkmsacc@outlook.com>
2024-08-09 23:34:39 +0700

    Refactor StorageService to improve file handling
    This refactor enhances the StorageService class by integrating Spring annotations and services for better DI and file handling. The changes include moving to MultipartFile for uploads, adding various utility methods for loading and deleting files, and implementing security and validation checks. These improvements make the service more robust and easier to maintain.

--- src/main/java/io/github/chubbyhippo/updown/infrastructure/StorageService.java
+++ src/main/java/io/github/chubbyhippo/updown/infrastructure/StorageService.java
@@ -1,16 +1,35 @@
 package io.github.chubbyhippo.updown.infrastructure;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+import org.springframework.util.FileSystemUtils;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 import java.util.stream.Stream;
 
+@Service
 public class StorageService {
+
     private final Path rootLocation;
 
-    public StorageService(String location) {
-        this.rootLocation = Paths.get(location);
+    @Autowired
+    public StorageService(StorageProperties properties) {
+
+        if (properties.location().trim().isEmpty()) {
+            throw new StorageException("File upload location can not be Empty.");
+        }
+
+        this.rootLocation = Paths.get(properties.location());
     }
 
     public void init() {
@@ -21,41 +40,61 @@ public class StorageService {
         }
     }
 
-    public void store(String filename, byte[] fileContent) {
+    public void store(MultipartFile file) {
         try {
-            var destinationFile = rootLocation.resolve(Paths.get(filename))
+            if (file.isEmpty()) {
+                throw new StorageException("Failed to store empty file.");
+            }
+            Path destinationFile = this.rootLocation.resolve(
+                            Paths.get(Objects.requireNonNull(file.getOriginalFilename())))
                     .normalize().toAbsolutePath();
-            Files.write(destinationFile, fileContent);
+            if (!destinationFile.getParent().equals(this.rootLocation.toAbsolutePath())) {
+                // This is a security check
+                throw new StorageException(
+                        "Cannot store file outside current directory.");
+            }
+            try (InputStream inputStream = file.getInputStream()) {
+                Files.copy(inputStream, destinationFile,
+                        StandardCopyOption.REPLACE_EXISTING);
+            }
         } catch (IOException e) {
-            throw new StorageException("Failed to store file " + filename, e);
+            throw new StorageException("Failed to store file.", e);
         }
     }
 
-    public byte[] read(String filename) {
+    public Stream<Path> loadAll() {
         try {
-            var filePath = rootLocation.resolve(filename).normalize().toAbsolutePath();
-            return Files.readAllBytes(filePath);
-        } catch (IOException e) {
-            throw new StorageException("Failed to read file " + filename, e);
-        }
-    }
-
-    public void delete(String filename) {
-        try {
-            var filePath = rootLocation.resolve(filename).normalize().toAbsolutePath();
-            Files.deleteIfExists(filePath);
-        } catch (IOException e) {
-            throw new StorageException("Failed to delete file " + filename, e);
-        }
-    }
-
-    public Stream<Path> listFiles() {
-        try {
-            return Files.walk(rootLocation, 1)
-                    .filter(path -> !path.equals(rootLocation))
-                    .map(rootLocation::relativize);
+            return Files.walk(this.rootLocation, 1)
+                    .filter(path -> !path.equals(this.rootLocation))
+                    .map(this.rootLocation::relativize);
         } catch (IOException e) {
             throw new StorageException("Failed to read stored files", e);
         }
+
     }
+
+    public Path load(String filename) {
+        return rootLocation.resolve(filename);
+    }
+
+    public Resource loadAsResource(String filename) {
+        try {
+            Path file = load(filename);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new StorageFileNotFoundException(
+                        "Could not read file: " + filename);
+
+            }
+        } catch (MalformedURLException e) {
+            throw new StorageFileNotFoundException("Could not read file: " + filename, e);
+        }
+    }
+
+    public void deleteAll() {
+        FileSystemUtils.deleteRecursively(rootLocation.toFile());
+    }
+
 }
