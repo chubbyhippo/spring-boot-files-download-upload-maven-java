c592819b2e5a2bad22fc1d3520d3791a50639b9c
chubbyhippo <mkmsacc@outlook.com>
2024-08-10 08:31:01 +0700

    Add applicationRunner to initialize storage service
    Introduced `ApplicationRunner` bean to perform storage initialization on application startup. The runner deletes all existing storage data and reinitializes the storage service.

--- src/main/java/io/github/chubbyhippo/updown/UpDownApplication.java
+++ src/main/java/io/github/chubbyhippo/updown/UpDownApplication.java
@@ -1,9 +1,12 @@
 package io.github.chubbyhippo.updown;
 
 import io.github.chubbyhippo.updown.infrastructure.StorageProperties;
+import io.github.chubbyhippo.updown.infrastructure.StorageService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -13,4 +16,11 @@ public class UpDownApplication {
         SpringApplication.run(UpDownApplication.class, args);
     }
 
+    @Bean
+    ApplicationRunner applicationRunner(StorageService storageService) {
+        return args -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 }
