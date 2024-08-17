ddfb5eeba32fa32896141accb9193358daf6a22a
ch <mkmsacc@outlook.com>
2024-08-17 13:58:23 +0700

    Refactor StorageService interface for exception handling
    Updated the StorageService interface to include throws declarations for specific exceptions. This change improves error handling by explicitly signaling potential issues during initialization, file storage, loading, and resource retrieval.

--- src/main/java/io/github/chubbyhippo/updown/domain/StorageService.java
+++ src/main/java/io/github/chubbyhippo/updown/domain/StorageService.java
@@ -7,15 +7,15 @@ import java.nio.file.Path;
 import java.util.stream.Stream;
 
 public interface StorageService {
-    void init();
+    void init() throws StorageException;
 
-    void store(MultipartFile file);
+    void store(MultipartFile file) throws EmptyFileException, StorageException;
 
-    Stream<Path> loadAll();
+    Stream<Path> loadAll() throws StorageException;
 
     Path load(String filename);
 
-    Resource loadAsResource(String filename);
+    Resource loadAsResource(String filename) throws StorageFileNotFoundException;
 
     void deleteAll();
 }
