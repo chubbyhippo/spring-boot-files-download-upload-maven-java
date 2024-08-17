4e9430199abb817fd22c4d8ce814ddeda5e15470
ch <mkmsacc@outlook.com>
2024-08-17 16:33:39 +0700

    Add tests for file upload, listing, and download
    New tests cover multiple file uploads, listing files, and downloading files. Enhanced test setup with MockMvc and ObjectMapper to facilitate JSON handling and validation.

--- src/test/java/io/github/chubbyhippo/updown/presentation/FileControllerTest.java
+++ src/test/java/io/github/chubbyhippo/updown/presentation/FileControllerTest.java
@@ -1,20 +1,26 @@
 package io.github.chubbyhippo.updown.presentation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.chubbyhippo.updown.application.FileService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.doNothing;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(FileController.class)
 class FileControllerTest {
@@ -23,11 +29,13 @@ class FileControllerTest {
     private MockMvc mockMvc;
     @MockBean
     private FileService fileService;
+    @Autowired
+    private ObjectMapper jacksonObjectMapper;
 
     @Test
     @DisplayName("should upload a file")
     void shouldUploadAFile() throws Exception {
-        doNothing().when(fileService).uploadFile(any());
+
         var mockMultipartFile = new MockMultipartFile(
                 "file",
                 "hello.txt",
@@ -41,5 +49,63 @@ class FileControllerTest {
                 .andExpect(content().string("File uploaded successfully: hello.txt"));
     }
 
+    @Test
+    @DisplayName("should upload files")
+    void shouldUploadFiles() throws Exception {
+        var file1 = new MockMultipartFile(
+                "file",
+                "test1.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                "test1".getBytes()
+        );
+
+        var file2 = new MockMultipartFile(
+                "file",
+                "test2.txt",
+                MediaType.TEXT_PLAIN_VALUE,
+                "test2".getBytes()
+        );
+        mockMvc.perform(multipart("/files")
+                        .file(file1)
+                        .file(file2))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Files uploaded successfully"));
+
+    }
+
+    @Test
+    @DisplayName("should list files")
+    void shouldListFiles() throws Exception {
+        when(fileService.listFiles()).thenReturn(Stream.of("testList1.txt", "testList2.txt"));
+
+        var mvcResult = mockMvc.perform(get("/files")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        var json = mvcResult.getResponse().getContentAsString();
+        var contentAsString = jacksonObjectMapper.readValue(json, String[].class);
+        var sorted = Arrays.stream(contentAsString).sorted().toArray();
+
+        assertThat(sorted).isEqualTo(new String[]{"testList1.txt", "testList2.txt"});
+
+    }
+
+    @Test
+    @DisplayName("should download file")
+    void shouldDownloadFile() throws Exception {
+
+        var filename = "testfile.txt";
+        var mockResource = mock(Resource.class);
+
+        when(mockResource.getFilename()).thenReturn(filename);
+        when(fileService.loadAsResource(filename)).thenReturn(mockResource);
+
+        mockMvc.perform(get("/files/{filename}", filename))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_OCTET_STREAM))
+                .andExpect(header().string("Content-Disposition",
+                        "attachment; filename=" + "\"" + filename + "\""));
+    }
 
 }
