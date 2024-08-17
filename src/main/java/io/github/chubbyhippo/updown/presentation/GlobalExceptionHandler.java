a46f72c602d4599205995f639f23c9fab2aa6a80
ch <mkmsacc@outlook.com>
2024-08-17 14:15:56 +0700

    Add handlers for storage-related exceptions
    Introduce exception handlers for StorageException and StorageFileNotFoundException in GlobalExceptionHandler. Handle StorageException with an HTTP 500 status and StorageFileNotFoundException with an HTTP 404 status.

--- src/main/java/io/github/chubbyhippo/updown/presentation/GlobalExceptionHandler.java
+++ src/main/java/io/github/chubbyhippo/updown/presentation/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package io.github.chubbyhippo.updown.presentation;
 
 import io.github.chubbyhippo.updown.domain.EmptyFileException;
+import io.github.chubbyhippo.updown.domain.StorageException;
+import io.github.chubbyhippo.updown.domain.StorageFileNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -13,6 +15,18 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public String handleEmptyFileException(EmptyFileException emptyFileException) {
         return emptyFileException.getMessage();
+    }
+
+    @ExceptionHandler(StorageException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleStorageException(StorageException storageException) {
+        return storageException.getMessage();
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleStorageFileNotFoundException(StorageFileNotFoundException storageFileNotFoundException) {
+        return storageFileNotFoundException.getMessage();
     }
 
 }
