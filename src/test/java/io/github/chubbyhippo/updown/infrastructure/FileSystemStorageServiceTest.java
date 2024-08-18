3b5d86715901f39b5b96a61fcae36f867d4af412
chubbyhippo <mkmsacc@outlook.com>
2024-08-18 16:11:58 +0700

    Add test for empty root path exception in FileSystemStorageService
    Introduce a unit test to ensure that FileSystemStorageService throws a StorageException when initialized with an empty root path. This improvement helps to validate the error handling during initialization.

--- src/test/java/io/github/chubbyhippo/updown/infrastructure/FileSystemStorageServiceTest.java
+++ src/test/java/io/github/chubbyhippo/updown/infrastructure/FileSystemStorageServiceTest.java
@@ -1,7 +1,22 @@
 package io.github.chubbyhippo.updown.infrastructure;
 
-import static org.junit.jupiter.api.Assertions.*;
+import io.github.chubbyhippo.updown.domain.StorageException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@ExtendWith(MockitoExtension.class)
 class FileSystemStorageServiceTest {
+
+
+    @Test
+    @DisplayName("should throw exception when root path is empty")
+    void shouldThrowExceptionWhenRootPathIsEmpty() {
+        assertThatThrownBy(() -> new FileSystemStorageService(new StorageProperties("")))
+                .isInstanceOf(StorageException.class);
+    }
 
 }
