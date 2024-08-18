0250c9437a104775459fb022f3cf4013fa88c207
chubbyhippo <mkmsacc@outlook.com>
2024-08-18 16:25:45 +0700

    Add test for exception when directory creation fails
    This commit includes a new test case in `FileSystemStorageServiceTest` to ensure that an `IOException` during directory creation results in a `StorageException`. This improves the robustness of the storage service by verifying its behavior in failure scenarios.

--- src/test/java/io/github/chubbyhippo/updown/infrastructure/FileSystemStorageServiceTest.java
+++ src/test/java/io/github/chubbyhippo/updown/infrastructure/FileSystemStorageServiceTest.java
@@ -6,7 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mockStatic;
 
 @ExtendWith(MockitoExtension.class)
 class FileSystemStorageServiceTest {
@@ -18,6 +24,24 @@ class FileSystemStorageServiceTest {
         assertThatThrownBy(() -> new FileSystemStorageService(new StorageProperties("")))
                 .isInstanceOf(StorageException.class)
                 .hasMessage("File upload location can not be Empty.");
+
+    }
+
+    @Test
+    @DisplayName("should throw exception when cannot create dir")
+    void shouldThrowExceptionWhenCannotCreateDir() {
+
+        try (var filesMockedStatic = mockStatic(Files.class)) {
+            filesMockedStatic.when(() -> Files.createDirectories(any(Path.class)))
+                    .thenThrow(new IOException());
+
+            var service = new FileSystemStorageService(new StorageProperties("test"));
+
+            assertThatThrownBy(service::init)
+                    .isInstanceOf(StorageException.class)
+                    .hasMessage("Could not initialize storage");
+
+        }
 
     }
 
