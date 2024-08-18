97b9ef1d4736ecba33c2253650a56b6bdd7ab1db
chubbyhippo <mkmsacc@outlook.com>
2024-08-18 16:33:10 +0700

    Add test for handling empty file upload
    This commit introduces a new test case to ensure the FileSystemStorageService correctly throws an EmptyFileException when attempting to upload an empty file. The TempDir annotation is also added to provide a temporary directory for test isolation, ensuring a clean environment for each test run.

--- src/test/java/io/github/chubbyhippo/updown/infrastructure/FileSystemStorageServiceTest.java
+++ src/test/java/io/github/chubbyhippo/updown/infrastructure/FileSystemStorageServiceTest.java
@@ -1,22 +1,27 @@
 package io.github.chubbyhippo.updown.infrastructure;
 
+import io.github.chubbyhippo.updown.domain.EmptyFileException;
 import io.github.chubbyhippo.updown.domain.StorageException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.io.TempDir;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.any;
-import static org.mockito.Mockito.mockStatic;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class FileSystemStorageServiceTest {
 
+    @TempDir
+    private Path tempDir;
 
     @Test
     @DisplayName("should throw exception when root path is empty")
@@ -35,13 +40,28 @@ class FileSystemStorageServiceTest {
             filesMockedStatic.when(() -> Files.createDirectories(any(Path.class)))
                     .thenThrow(new IOException());
 
-            var service = new FileSystemStorageService(new StorageProperties("test"));
+            var service = new FileSystemStorageService(new StorageProperties(tempDir.toString()));
 
             assertThatThrownBy(service::init)
                     .isInstanceOf(StorageException.class)
                     .hasMessage("Could not initialize storage");
 
         }
+
+    }
+
+    @Test
+    @DisplayName("should throw exception when uploading an empty file")
+    void shouldThrowExceptionWhenUploadingAnEmptyFile() {
+
+        var service = new FileSystemStorageService(new StorageProperties(tempDir.toString()));
+
+        var multipartFile = mock(MultipartFile.class);
+        when(multipartFile.isEmpty()).thenReturn(true);
+
+        assertThatThrownBy(() -> service.store(multipartFile))
+                .isInstanceOf(EmptyFileException.class)
+                .hasMessage("Failed to store empty file.");
 
     }
 
