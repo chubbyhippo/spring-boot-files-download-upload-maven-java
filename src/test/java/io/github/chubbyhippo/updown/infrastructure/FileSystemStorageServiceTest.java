9ee1457f0376102ddf3c68d61db231f931fa3169
ch <mkmsacc@outlook.com>
2024-08-19 16:01:51 +0700

    Refactor test for clearer setup
    Reorganized the variable initialization within the test method by extracting `StorageProperties` into a separate variable. This change enhances code readability and maintains consistency with other test cases.

--- src/test/java/io/github/chubbyhippo/updown/infrastructure/FileSystemStorageServiceTest.java
+++ src/test/java/io/github/chubbyhippo/updown/infrastructure/FileSystemStorageServiceTest.java
@@ -26,7 +26,8 @@ class FileSystemStorageServiceTest {
     @Test
     @DisplayName("should throw exception when root path is empty")
     void shouldThrowExceptionWhenRootPathIsEmpty() {
-        assertThatThrownBy(() -> new FileSystemStorageService(new StorageProperties("")))
+        StorageProperties properties = new StorageProperties("");
+        assertThatThrownBy(() -> new FileSystemStorageService(properties))
                 .isInstanceOf(StorageException.class)
                 .hasMessage("File upload location can not be Empty.");
 
