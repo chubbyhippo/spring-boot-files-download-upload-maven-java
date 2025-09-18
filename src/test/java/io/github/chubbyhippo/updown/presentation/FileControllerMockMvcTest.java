c5fb0f546993e3fa5bf4557560f971e070d884b9
chubbyhippo <mkmsacc@outlook.com>
2025-09-18 20:19:23 +0700

    Reorganize static imports in `FileControllerMockMvcTest` for improved clarity and consistency

--- src/test/java/io/github/chubbyhippo/updown/presentation/FileControllerMockMvcTest.java
+++ src/test/java/io/github/chubbyhippo/updown/presentation/FileControllerMockMvcTest.java
@@ -20,7 +20,9 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FileController.class)
 class FileControllerMockMvcTest {
