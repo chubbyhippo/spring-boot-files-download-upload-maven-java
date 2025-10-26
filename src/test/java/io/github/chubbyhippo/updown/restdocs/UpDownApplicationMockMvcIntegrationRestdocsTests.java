fc67f56d35db2c4b95a1024aee90dd74da3de9e0
chubbyhippo <mkmsacc@outlook.com>
2025-10-26 08:37:36 +0700

    Set `outputDir` attribute in `@AutoConfigureRestDocs` to `target/generated-snippets/mockmvc` in `UpDownApplicationMockMvcIntegrationRestdocsTests`

--- src/test/java/io/github/chubbyhippo/updown/restdocs/UpDownApplicationMockMvcIntegrationRestdocsTests.java
+++ src/test/java/io/github/chubbyhippo/updown/restdocs/UpDownApplicationMockMvcIntegrationRestdocsTests.java
@@ -43,7 +43,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@AutoConfigureRestDocs
+@AutoConfigureRestDocs(outputDir = "target/generated-snippets/mockmvc")
 @Execution(ExecutionMode.SAME_THREAD)
 class UpDownApplicationMockMvcIntegrationRestdocsTests {
 
